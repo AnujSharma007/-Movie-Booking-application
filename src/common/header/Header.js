532d9b3c9a8fd0372f28a4e756b70a558a1d0dd7
Anuj Sharma <anujankur13@gmail.com>
2022-02-05 17:24:26 +0530

    Checkpoint2

---
+++ src/common/header/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Header.css'
+class Header extends Component {
+    state = {  } 
+    render() { 
+        return (<div className='header'>
+            <img src='src\assets\logo.svg' className='HeaderImg'></img>
+        </div>);
+    }
+}
+ 
+export default Header;
