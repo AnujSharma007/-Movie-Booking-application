fc739cc76538224465d4350c7463d695345c42cf
Anuj Sharma <anujankur13@gmail.com>
2022-02-08 14:22:37 +0530

    Checkpoint3

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
-import './Header.css'
+import './Header.css';
+
+
 class Header extends Component {
     state = {  } 
     render() { 
         return (<div className='header'>
-            <img src='src\assets\logo.svg' className='HeaderImg'></img>
-        </div>);
+                     <img src='src\assets\logo.svg' className='HeaderImg'></img>
+                </div>);
     }
 }
  
 export default Header;
+
+
--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import Header from './common/header/Header';
 import Home from './screens/Home/Home';
 
 ReactDOM.render(
-    <Header />,
+    <Home />,
   document.getElementById('root')
 );
+
+
 
