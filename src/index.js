cf06f9d7835361276380bff38f6db74388bb3cff
Anuj Sharma <anujankur13@gmail.com>
2022-02-12 14:16:13 +0530

    Checkpoint5

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './common/header/Header';
+import Details from './screens/Home/details/Details';
 import Home from './screens/Home/Home';
 
-ReactDOM.render(
-    <Home />,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//     <Home />,
+//   document.getElementById('root')
+// );
 
+ReactDOM.render(
+  <Details />,
+document.getElementById('root')
+);
 
 
